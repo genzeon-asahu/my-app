f54160279a9132cd84e0278592b288b3010b9ad9
Anil Sahu <anil.sahu@genzeon.com>
2025-02-06 16:57:44 +0530

    Add testing message for pull request label verification in App component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Testing labels when pull request is created
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
