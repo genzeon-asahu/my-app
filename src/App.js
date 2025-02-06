e2b7a928dca1199980a9254940156ed4ae45a482
Anil Sahu <anil.sahu@genzeon.com>
2025-02-06 16:45:40 +0530

    Update App component to reflect code modifications

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Code has been modified to see the changes.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
